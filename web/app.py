1a15a5dfcc94368d0390af85c85fea8be06e5c74
Jacek Bogocz <jacek.bogocz@gmail.com>
2020-05-24 22:58:54 +0200

    check if user exists in database

--- web/app.py
+++ web/app.py
@@ -14,3 +14,11 @@ client = MongoClient('mongodb://db:27017')
 db = client.BankAPI
 users = db['Users']
 
+# Check if user exists in database
+def UserExist(username):
+    # Find query username in MongoDB
+    if users.find({'Username': username}).count() == 0:
+        return False
+    else:
+        return True
+

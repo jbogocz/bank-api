89b87d41bb49caf6da6008dec776afff8dd3d5cb
Jacek Bogocz <jacek.bogocz@gmail.com>
2020-05-25 00:38:19 +0200

    add resources to the API

--- web/app.py
+++ web/app.py
@@ -251,3 +251,13 @@ class PayLoan(Resource):
 
         return jsonify(generateReturnDictionary(200, "Loan Paid"))
 
+# Add resources to the API
+api.add_resource(Register, '/register')
+api.add_resource(Add, '/add')
+api.add_resource(Transfer, '/transfer')
+api.add_resource(Balance, '/balance')
+api.add_resource(TakeLoan, '/takeloan')
+api.add_resource(PayLoan, '/payloan')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
